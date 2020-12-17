fdfa23f6bf28a81e4234465e38798241bc01c507
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2020-12-17 18:23:05 +0530

    added new branch

---
+++ demotwo/src/demotwo/sampleforbranch.java
@@ -0,0 +1,15 @@
+package demotwo;
+
+public class sampleforbranch {
+	public void disp()
+	{
+		System.out.println("hi");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("branchning");
+
+	}
+
+}
